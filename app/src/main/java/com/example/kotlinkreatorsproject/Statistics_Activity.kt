68ca96129637d43a42424e11519e7493fe2a82f1
Lucas Mello <lucasmmello03@gmail.com>
2024-08-12 18:31:36 -0300

    Início desenvolvimento tela Statistics_Activity

---
+++ app/src/main/java/com/example/kotlinkreatorsproject/Statistics_Activity.kt
@@ -0,0 +1,24 @@
+package com.example.kotlinkreatorsproject
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.kotlinkreatorsproject.databinding.ActivityStatisticsBinding
+
+class Statistics_Activity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityStatisticsBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
